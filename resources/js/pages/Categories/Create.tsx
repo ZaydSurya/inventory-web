1a90b6ef32512d1886db3b5d02db3556fc25b342
Zayd Surya Marthana <zaydsuryamarthana@gmail.com>
2025-09-01 18:21:17 +0700

    input

--- resources/js/pages/Categories/Create.tsx
+++ resources/js/pages/Categories/Create.tsx
@@ -5,6 +5,7 @@ import { Head, Link, usePage, useForm } from '@inertiajs/react';
 import { Button } from '@/components/ui/button';
 import { Label } from '@/components/ui/label';
 import { Input } from '@/components/ui/input';
+import { Heading } from '@/components/heading';
 
 const breadcrumbs: BreadcrumbItem[] = [
     {
@@ -26,12 +27,13 @@ export default function Categories() {
     return (
         <AppLayout breadcrumbs={breadcrumbs}>
             <Head title="Create new Category" />
-            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-4 overflow-x-auto">
-                <div className="relative min-h-[100vh] flex-1 overflow-hidden rounded-xl">
+            <div className="flex h-full flex-1 flex-col gap-4 rounded-xl p-5 overflow-x-auto">
+                <div className="relative min-h-[10vh] flex-1 overflow-hidden rounded-xl p-5 bg-white shadow-md">
                     <div className="m-4">
                         <form onSubmit={handleSubmit} className='space-y-4'>
                             <div className='gap-5 mb-4'>
-                                <Label className='product'>Nama Kategori</Label>
+                                <Heading></Heading>
+                                <Label className='font-light'>Nama Kategori</Label>
                                 <Input placeholder='Ketikan kategori' value={data.name} onChange={(e) => setData('name', e.target.value)}></Input>
                             </div>
                             <Button type='submit'>Buat baru</Button>
